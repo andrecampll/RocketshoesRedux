d06c47adc9b64cbccdf3c6f4d1ad6b1840f9c07b
André Victor <andrevictor50@gmail.com>
2020-03-06 12:03:39 -0300

    API Fake

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-import { Container } from './styles';
+import { Container,
+ List,
+ Product,
+ Image,
+ Name,
+ Price,
+ AddToCartButton,
+ AddToCartText,
+} from './styles';
 
-export default function Home() {
-  return <Container />;
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    const data = {
+      id: response.data.id,
+      image: response.data.image,
+      price: response.data.price,
+      title: response.data.title
+    };
+
+    this.setState({
+      products: [...products, data],
+    })
+  }
+  
+  render() {
+    const {products} = this.state;
+
+    return (
+      <Container>
+        <List 
+          data={products}
+          keyExtractor={product => product.id}
+          renderItem={({ item }) => (
+            <Product>
+              <Image source={{ uri: item.image }}/>
+              <Name>{item.title}</Name>
+              <Price>{item.price}</Price>
+  
+              <AddToCartButton>
+                <AddToCartText>ADICIONAR A0 CARRINHO</AddToCartText>
+              </AddToCartButton>
+            </Product>
+          )}
+        />
+      </Container>
+    );
+  }
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
   background-color: #191920;
 `;
+
+export const List = styled.FlatList``;
+export const Product = styled.View``;
+export const Image = styled.Image``;
+export const Name = styled.Text``;
+export const Price = styled.Text``;
+export const AddToCartButton = styled(RectButton)``;
+export const AddToCartText = styled.Text``;
